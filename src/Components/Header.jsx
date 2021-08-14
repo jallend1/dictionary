9adf9b8a8062d3ddecbd4114b3d2eb79c835be92
jallend1 <jallend1@gmail.com>
2021-08-14 12:35:53 -0700

    Adds Header component and converts to dark theme

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <div className="title">
+            Word Lookup
+            </div>
+        </header>
+    )
+}
+
+export default Header
