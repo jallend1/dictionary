5d069dbb39c44efacd63a27b4a8689672cbdfc21
jallend1 <jallend1@gmail.com>
2021-08-13 17:22:04 -0700

    Removes extra create-react-app code and adds input to main page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { useEffect, useState } from 'react';
+import { TextField } from '@material-ui/core';
 
 function App() {
   const [word, setWord] = useState('hello');
   const getWord = (incoming) => {
-    const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + incoming
-    fetch(url).then(res => res.json()).then((data) =>setWord(data[0].word));
-  }
+    const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + incoming;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setWord(data[0].word));
+  };
 
-  useEffect(() => getWord('howdy'), [])
+  useEffect(() => getWord('howdy'), []);
   return (
-    <div className="App">
-      {word} there
+    <div className='App'>
+      <header>
+        <TextField />
+      </header>
+      <main>{word} there</main>
     </div>
   );
 }
