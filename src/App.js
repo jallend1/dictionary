0642112e12d66ef5a1ea4fe153ec49d4e9bdb2f0
jallend1 <jallend1@gmail.com>
2021-08-14 11:11:57 -0700

    Adds form and stores audio and definitions in state

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import { useEffect, useState } from 'react';
 import { TextField } from '@material-ui/core';
 
 function App() {
-  const [word, setWord] = useState('hello');
-  const getWord = (incoming) => {
+  const [word, setWord] = useState('');
+  const [definition, setDefinition] = useState('');
+  const [audio, setAudio] = useState('');
+
+  const getWord = (e, incoming) => {
+    e.preventDefault();
     const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + incoming;
     fetch(url)
       .then((res) => res.json())
-      .then((data) => setWord(data[0].word));
+      .then((data) => {
+        console.log(data[0]);
+        setDefinition(data[0].meanings[0].definitions[0].definition);
+        setAudio(data[0].phonetics[0].audio);
+      });
   };
 
-  useEffect(() => getWord('howdy'), []);
+  // useEffect(() => getWord('howdy'), []);
   return (
     <div className='App'>
       <header>
-        <TextField />
+        <form onSubmit={(e) => getWord(e, word)}>
+          <TextField value={word} onChange={(e) => setWord(e.target.value)} />
+        </form>
       </header>
       <main>{word} there</main>
     </div>
